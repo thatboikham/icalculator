9f280ad99a2e4ab295d9bdbaaee013b2d971127a
thatboikham <khamjapher1@gmail.com>
2023-09-28 03:35:38 +0300

    feat: reduced fonts size

--- calc.js
+++ calc.js
@@ -1,7 +1,9 @@
 const numbers = document.querySelectorAll(".number")
 const resultDIv = document.querySelector("#resultSpan")
 const addition = document.querySelector('.value_addition')
+const multiplication = document.querySelector('.value_multyplication')
 const equals = document.querySelector('.value_equalsign')
+const clear = document.querySelector('.value_clear')
 
 
 let firstNumber = "";
@@ -11,9 +13,11 @@ let operation = null;
 //loop through the numbers to get their values
 
 function getFirstNumber(e){
-    if(firstNumber.length < 8){
+    if(firstNumber.length < 10){
         const clikedNumber = this.innerHTML;
         firstNumber += clikedNumber;
+    }else if(firstNumber.length >= 5){
+        resultDIv.style.cssText = "font-size: 30px";
     }
     resultDIv.innerHTML = parseInt(firstNumber).toLocaleString();
 }
@@ -29,6 +33,10 @@ function add(){
     let sum = parseInt(firstNumber) + parseInt(secondNumber);
     resultDIv.innerHTML = sum;
 }
+function multiply(){
+    let product = parseInt(firstNumber) * parseInt(secondNumber);
+    resultDIv.innerHTML = product;
+}
 
 numbers.forEach(number => {
     number.addEventListener("click", function () {
@@ -37,6 +45,7 @@ numbers.forEach(number => {
         } else {
             getSecondNumber.call(this);
         }
+        clear.innerHTML = "C"
     });
 });
 
@@ -44,10 +53,24 @@ addition.addEventListener("click", () => {
     operation = "addition";
     resultDIv.innerHTML = "";
 });
+multiplication.addEventListener("click", () => {
+    operation = "multiplication"
+    resultDIv.innerHTML = "";
+})
 
 // Add click event listener for the equals button to perform the operation
 equals.addEventListener("click", () => {
     if (operation === "addition") {
         add();
     }
+    else if(operation === "multiplication"){
+        multiply();
+    }
+});
+clear.addEventListener("click", () => {
+    clear.innerHTML = "AC"
+    firstNumber = "";
+    secondNumber = "";
+    operation = null;
+    resultDIv.innerHTML = 0;
 });
